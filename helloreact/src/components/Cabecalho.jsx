c36c5337cd5a98dfb915b3f39622ac4e2e04d3d0
monopolizante <robsonlaurentino15@gmail.com>
2026-02-11 22:18:00 -0300

    Adiciona primeiro projeto node

---
+++ helloreact/src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cabecalho() {
+    return (
+        <header>
+            <p>Oi, senhor aura</p>
+        </header>
+    )
+}
+
+export default Cabecalho
---
+++ helloreact/src/components/Corpo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Corpo() {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque corrupti quas ad dolore dolor, nulla et. Optio aliquam magnam beatae, quas commodi, corporis asperiores quisquam dolor quae porro soluta consequuntur!</p>
+        </div>
+    )
+}
+
+export default Corpo
