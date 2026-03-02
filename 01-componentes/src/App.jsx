204c3f055179efc6f2da59874cc4a975fcff126a
Monopolizante <robsonlaurensan@gmail.com>
2026-03-02 19:35:21 -0300

    Utiliza os componentes

--- 01-componentes/src/App.jsx
+++ 01-componentes/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import AdivinheONumero from './components/AdivinheONumero.jsx'
 import DiaDaSemana from './components/DiaDaSemana.jsx'
+import CalculoAlbergue from "./components/CalculoAlbergue.jsx"
+import Feira from './components/Feira.jsx'
+import Voto from './components/Voto.jsx'
 function App() {
 
   return (
     <>
       <h1>01-Componentes</h1>
+      < Voto />
+      <Feira />
+      <CalculoAlbergue />
       <AdivinheONumero />
       <DiaDaSemana />
     </>
