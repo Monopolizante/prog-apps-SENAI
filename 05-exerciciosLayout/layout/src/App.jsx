ed2b284c9708faed025825d701d9bd9099692d93
monopolizante <robsonlaurensan@gmail.com>
2026-04-01 22:15:34 -0300

    Começo dos exercicios de layout

---
+++ 05-exerciciosLayout/layout/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className={"cont-app"}>
+      <h1>Food Park Parei Comi!</h1>
+      <main className={"cont-main"}>
+      </main>
+    </div>
+    </>
+  )
+}
+
+export default App
